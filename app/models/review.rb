141a7e7906c1eb0873c816ba3c501d43ff7450dc
Min Xuan Ng <hi@ngminxuan.com>
2021-09-09 11:38:29 +0800

    created models and validations

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :content, presence: true
+  validates :restaurant_id, presence: true
+end
